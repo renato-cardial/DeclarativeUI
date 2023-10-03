d5457698712115edd20af6841c0f1660d85d4e03
Renato Cardial <rcardial.souza@gmail.com>
2023-10-02 21:10:03 -0300

    Initial Commit: Prepared package, project and workspace

---
+++ DeclarativeUISample/DeclarativeUISample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeclarativeUISample
+//
+//  Created by Renato Cardial on 10/2/23.
+//
+
+import UIKit
+import DeclarativeUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(DeclarativeUI().text)
+    }
+
+
+}
+
